eecc161aaafb3cd7ac3f18bd3b7b133a44cc46bb
meenatchisundharam <121151601+meenatchisundharam@users.noreply.github.com>
2024-05-15 12:53:59 +0530

    create pricecard.vue

---
+++ taiwindcss_youtube/pages/pricecard.vue
@@ -0,0 +1,78 @@
+<template>
+<div class="p-5 w-full flex flex-wrap gap-10 justify-center items-center min-h-screen bg-blue-500"
+>
+<div
+class="w-72 h-96 text-gray-300 rounded-lg bg-cover bg-blue-50 px-8 py-4"
+>
+<h3 class="text-lg text-blue-600 font-bold">Start-up</h3>
+<h3 class="text-lg text-blue-600 font-extrabold">Free forever</h3>
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur cupiditate .</p>
+<ul class="space-y-2">
+<li>
+  <i class="fa-solid fa-check mr-2 text-blue-600"></i><span >50GB file storage </span>
+</li>
+<li>
+  <i class="fa-solid fa-check mr-2 text-blue-600"></i><span>500 Emails per day </span>
+</li>
+<li>
+  <i class="fa-solid fa-check mr-2 text-blue-600"></i><span >10 workflow</span>
+</li>
+<li>
+ <i class="fa-solid fa-check mr-2 text-blue-600"></i> <span>Deal pipeline </span>
+</li>
+
+</ul>
+<div class="mt-5 text-center border border-blue-600 rounded-3xl text-blue-600 py-2 px-4 hover:bg-blue-600 hover:text-white">request a demo</div>
+
+</div>
+<div
+class="w-72 h-96 text-gray-300 rounded-lg bg-cover bg-blue-50 px-8 py-4"
+>
+<h3 class="text-lg text-blue-600 font-bold">Start-up</h3>
+<h3 class="text-lg text-blue-600 font-extrabold">Free forever</h3>
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur cupiditate .</p>
+<ul class="space-y-2">
+<li>
+  <i class="fa-solid fa-check mr-2 text-blue-600"></i><span >50GB file storage </span>
+</li>
+<li>
+  <i class="fa-solid fa-check mr-2 text-blue-600"></i><span>500 Emails per day </span>
+</li>
+<li>
+  <i class="fa-solid fa-check mr-2 text-blue-600"></i><span >10 workflow</span>
+</li>
+<li>
+ <i class="fa-solid fa-check mr-2 text-blue-600"></i> <span>Deal pipeline </span>
+</li>
+
+</ul>
+<div class="mt-5 text-center border border-blue-600 rounded-3xl text-blue-600 py-2 px-4 hover:bg-blue-600 hover:text-white">request a demo</div>
+
+</div>
+<div
+class="w-72 h-96 text-gray-300 rounded-lg bg-cover bg-blue-50 px-8 py-4"
+>
+<h3 class="text-lg text-blue-600 font-bold">Start-up</h3>
+<h3 class="text-lg text-blue-600 font-extrabold">Free forever</h3>
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur cupiditate .</p>
+<ul class="space-y-2">
+<li>
+  <i class="fa-solid fa-check mr-2 text-blue-600"></i><span >50GB file storage </span>
+</li>
+<li>
+  <i class="fa-solid fa-check mr-2 text-blue-600"></i><span>500 Emails per day </span>
+</li>
+<li>
+  <i class="fa-solid fa-check mr-2 text-blue-600"></i><span >10 workflow</span>
+</li>
+<li>
+ <i class="fa-solid fa-check mr-2 text-blue-600"></i> <span>Deal pipeline </span>
+</li>
+
+</ul>
+<div class="mt-5 text-center border border-blue-600 rounded-3xl text-blue-600 py-2 px-4 hover:bg-blue-600 hover:text-white">request a demo</div>
+
+</div>
+</div>
+
+</template>
