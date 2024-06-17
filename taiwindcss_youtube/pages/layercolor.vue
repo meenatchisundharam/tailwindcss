bf3b17f17a92a94cbd5219cde1c07f2c0ce67f1b
meenatchisundharam <121151601+meenatchisundharam@users.noreply.github.com>
2024-06-17 19:33:15 +0530

    layer

---
+++ taiwindcss_youtube/pages/layercolor.vue
@@ -0,0 +1,11 @@
+<template>
+	<div class=" relative">
+  <div class="border-4 border-gray-400 border-opacity-24 rounded-xl bg-black w-full h-full">
+    <!-- ... your content here ... -->
+    
+  </div>
+  <div class="absolute top-96 inset-y-20 right-0 w-1 h-20 bg-gradient-to-t from-blue-500/65 to-transparent"></div>
+  <div class="absolute inset-y-20 left-0 w-1 h-20 bg-gradient-to-b from-blue-500/65 to-transparent"></div>
+</div>
+ 
+</template>
